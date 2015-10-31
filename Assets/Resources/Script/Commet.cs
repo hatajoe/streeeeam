5351d9d3c939120d53493f5a5804b90a2ee69376
Yusuke Hatanaka <arbalestimp@gmail.com>
2015-10-31 11:58:16 +0900

    Add commet randomize

--- Assets/Resources/Script/Commet.cs
+++ Assets/Resources/Script/Commet.cs
@@ -4,36 +4,35 @@ using System.Collections;
 public class Commet : MonoBehaviour {
 
 	public GameObject planet;
+	private float velocity;
 
-	Commet(Vector3 pos, float velocity) {
-		this.transform.localPosition = pos;
+	public void Init(float y, float initialDelta, float velocity, float scale) {
+		this.velocity = velocity;
+		this.transform.localPosition = new Vector3 (0f, y, 0f);
+		this.transform.localScale = new Vector3 (scale, scale);
+		this.Move (initialDelta);
 	}
 
 	// Use this for initialization
 	void Start () {
-		this.transform.localPosition = new Vector3 (0f, 1.8f, 0f);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		this.Move (this.velocity);
+	}
+
+	private void Move(float delta) {
 		// Z軸を自オブジェクトの回転を使って回転する。
 		Vector3 forward = transform.rotation * Vector3.forward;
 		// 念のために正規化する。
 		forward.Normalize();
-
-		Quaternion q = Quaternion.AngleAxis(1, forward);
+		
+		Quaternion q = Quaternion.AngleAxis(this.velocity * -10f, forward);
 		// 自オブジェクトを回転する。
 		transform.rotation = q * transform.rotation;
-
-		transform.RotateAround(planet.transform.position, transform.forward, 0.1f);
-
-		/*
-		// 親オブジェクトの位置を取得する。
-		var parentPosition = planet.transform.position;
-		// 親オブジェクトから自オブジェクトへ向かうベクトルを作成する。
-		var dir = transform.position - parentPosition;
-		// 自オブジェクトの位置を計算するために、原点でベクトルを回転後、親オブジェクトの座標で移動させる。
-		transform.position = q * dir + parentPosition;
-		*/
+		
+		transform.RotateAround(planet.transform.position, transform.forward * -1.0f, delta);
 	}
 }
---
+++ Assets/Resources/Script/Space.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class Space : MonoBehaviour {
+
+	public static int COMMENT_COUNT = 30;
+	public static float COMMET_INITIAL_POS_Y_MIN = 1.55f;
+	public static float COMMET_INITIAL_POS_Y_MAX = 1.90f;
+	public static float COMMET_INITIAL_DELTA_MIN = 300.0f;
+	public static float COMMET_INITIAL_DELTA_MAX = 1050.0f;
+	public static float COMMET_VELOCITY_MIN = 0.05f;
+	public static float COMMET_VELOCITY_MAX = 0.10f;
+	public static float COMMET_SCALE_MIN = 0.8f;
+	public static float COMMET_SCALE_MAX = 1.2f;
+
+	public GameObject commet;
+	private List<Commet> commets;
+
+	// Use this for initialization
+	void Start () {
+		this.commets = new List<Commet> ();
+		for (int i = 0; i < COMMENT_COUNT; i++) {
+			float y = this.GetRandomByRange(COMMET_INITIAL_POS_Y_MIN, COMMET_INITIAL_POS_Y_MAX);
+			float initialDelta = this.GetRandomByRange(COMMET_INITIAL_DELTA_MIN, COMMET_INITIAL_DELTA_MAX);
+			float velocity = this.GetRandomByRange(COMMET_VELOCITY_MIN, COMMET_VELOCITY_MAX);
+			float scale  = this.GetRandomByRange(COMMET_SCALE_MIN, COMMET_SCALE_MAX);
+
+			GameObject obj = (GameObject)Instantiate (commet);
+
+			Commet c = obj.GetComponentInChildren<Commet>();
+			c.Init(y, initialDelta, velocity, scale);
+			this.commets.Add (c);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private float GetRandomByRange(float min, float max) {
+		return Random.value * (max - min) + min;
+	}
+}
