232a08761e7e379750579b6565be55e38d7ac844
Yusuke Hatanaka <arbalestimp@gmail.com>
2015-10-31 21:50:08 +0900

    Modify phsyics settings

--- Assets/Resources/Script/TrackingCamera.cs
+++ Assets/Resources/Script/TrackingCamera.cs
@@ -3,7 +3,19 @@ using System.Collections;
 
 public class TrackingCamera : MonoBehaviour {
 
-	public GameObject player;
+	public Player player;
+	private Rigidbody rigidbody = null;
+
+	// Use this for initialization
+	void Awake()
+	{
+		Rigidbody rigidbody = this.GetComponentInChildren<Rigidbody>(); 
+		
+		if ( rigidbody )
+		{
+			this.rigidbody = rigidbody;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -17,10 +29,26 @@ public class TrackingCamera : MonoBehaviour {
 			this.player.transform.localPosition.y,
 			this.transform.localPosition.z
 		);
-		this.transform.Rotate(
-			0,
-			0,
-			this.player.transform.rotation.z	
+		/*
+		if (this.player.IsTouched) {
+			if (this.transform.position.z > -8.72f) {
+				this.GetComponent<Rigidbody>().AddForce(Vector3.forward * -0.1f * this.player.boost, ForceMode.Impulse);
+			}
+		} else {
+			if (this.transform.position.z < -4.72f) {
+				this.GetComponent<Rigidbody>().AddForce(Vector3.forward * this.player.boost * 0.08f, ForceMode.Impulse);
+			} else {
+				this.GetComponent<Rigidbody>().AddForce(Vector3.forward * this.player.boost * -0.08f, ForceMode.Impulse);
+			}
+		}
+		*/
+
+		/*
+		this.transform.rotation = Quaternion.Euler (
+			this.transform.localRotation.x,
+			this.transform.localRotation.y,
+			this.player.transform.localPosition.z
 		);
+		*/
 	}
 }
