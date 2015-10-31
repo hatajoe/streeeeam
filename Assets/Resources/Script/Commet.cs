2e50b996e582ad824c2519cd3358a776533779f2
yuki maeta <pattern.ao@gmail.com>
2015-10-31 17:54:00 +0900

    Update Main Scene.

--- Assets/Resources/Script/Commet.cs
+++ Assets/Resources/Script/Commet.cs
@@ -38,7 +38,7 @@ public class Commet : MonoBehaviour {
 
 	private IEnumerator Crash()
 	{
-		yield return new WaitForSeconds(1.0f); 
+		yield return new WaitForSeconds(0.3f); 
 
 		Destroy(this.gameObject);
 	}
