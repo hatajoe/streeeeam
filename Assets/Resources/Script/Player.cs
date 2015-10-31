bb85fe48acc618637933ee83560b7f2d428ffc4d
yuki maeta <pattern.ao@gmail.com>
2015-10-31 12:22:34 +0900

    #9 Add player & touch panel.

---
+++ Assets/Resources/Script/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+					, ITouchPanelEventObserver
+{
+	//=== Inspector
+	public Vector3 boostForce = new Vector3();
+
+	//=== Properties
+	private Rigidbody rigidbody = null;
+
+
+	// Use this for initialization
+	void Awake()
+	{
+		Rigidbody rigidbody = this.GetComponentInChildren<Rigidbody>(); 
+
+		if ( rigidbody )
+		{
+			this.rigidbody = rigidbody;
+		}
+	}
+
+	void Start () 
+	{
+		TouchPanel.GetInstance().SetObserver(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//=== @interface ITouchPanelEventObserver 
+	public void Touching(TouchPanel panel)
+	{
+		Debug.Log("Touching");
+
+		this.rigidbody.AddForce(boostForce, ForceMode.Acceleration);
+	}
+
+	public void Clicked(TouchPanel panel)
+	{
+		// No action.
+	}
+
+}
---
+++ Assets/Resources/Script/TouchPanel.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public interface ITouchPanelEventObserver
+{
+	void Touching(TouchPanel panel); 
+	void Clicked(TouchPanel panel);
+}
+
+
+public class TouchPanel : MonoBehaviour 
+						, IPointerDownHandler
+						, IPointerUpHandler
+						, IPointerClickHandler
+{
+	[System.NonSerialized] public static TouchPanel s_Instance = null;
+
+	private List<ITouchPanelEventObserver> observers = null;
+	private bool touching = false;
+
+
+	public static TouchPanel GetInstance()
+	{
+		if ( TouchPanel.s_Instance )
+			return TouchPanel.s_Instance;
+
+		GameObject obj = GameObject.Find ("TouchPanel");
+
+		if ( obj )
+		{
+			TouchPanel.s_Instance = obj.GetComponentInChildren<TouchPanel>(); 
+		}
+
+		return TouchPanel.s_Instance;
+	}
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		observers = new List<ITouchPanelEventObserver>(); 
+	}
+
+	void FixedUpdate()
+	{
+		if ( touching == false )
+			return; 
+
+		if ( observers == null )
+			return; 
+		
+		foreach ( ITouchPanelEventObserver observer in observers )
+		{
+			observer.Touching(this);
+		}
+	}
+
+	public void SetObserver( ITouchPanelEventObserver observer )
+	{
+		if ( observers.Contains(observer) == true )
+			return; 
+
+		observers.Add(observer);
+	}
+	 
+	public void OnPointerDown (PointerEventData eventData) 
+	{
+		touching = true;
+	}
+
+	public void OnPointerUp (PointerEventData eventData) 
+	{
+		touching = false;
+	}
+
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		if ( observers == null )
+			return; 
+
+		foreach ( ITouchPanelEventObserver observer in observers )
+		{
+			observer.Clicked(this);
+		}
+	}
+}
