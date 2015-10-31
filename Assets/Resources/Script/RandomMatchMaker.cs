aa67f1a31e42711594c4c92d80597a0c194cd621
yuki maeta <pattern.ao@gmail.com>
2015-10-31 21:58:08 +0900

    Add photon library.

---
+++ Assets/Resources/Script/RandomMatchMaker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMatchMaker : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start()
+	{
+ 		PhotonNetwork.ConnectUsingSettings("0.1");
+		PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	}
+
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+}
