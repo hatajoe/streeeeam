12c3c035e615653b4e739c24402239ccbacfd408
yuki maeta <pattern.ao@gmail.com>
2015-10-31 22:15:48 +0900

    photon player & commit.

--- Assets/Resources/Script/PhotonObject.cs
+++ Assets/Resources/Script/PhotonObject.cs
@@ -11,8 +11,8 @@ public class PhotonObject : Photon.MonoBehaviour
 	{
 		if (!photonView.isMine)
 		{
-			transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
-			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+//			transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+//			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
 		}
 	}
 
